d853b6fed96b9d9dfd2334add2d15083dcce5803
Samuel Oliveira <samueldevbr@gmail.com>
2022-08-24 15:37:35 -0300

    fixing vulnerability issues

--- qanda/protected/controllers/DiscussController.php
+++ qanda/protected/controllers/DiscussController.php
@@ -1,10 +1,12 @@
 <?php
 
 $id = $_GET['id'];
-$conn = new PDO("mysql:host=localhost;dbname=whatever", "user", "password");
+$conn = new PDO("mysql:host=localhost;dbname=whatever", $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
 
-$stmt = $conn->prepare("SELECT * FROM users WHERE id = '$id'");
-$stmt->execute();
+$stmt = $conn->prepare("SELECT * FROM users WHERE id = :id");
+$stmt->execute([
+    "id" => $id
+]);
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
