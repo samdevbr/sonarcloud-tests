4e41cc57e4a210228205b635f872257c7ad6ac4f
Samuel Oliveira <samueldevbr@gmail.com>
2022-08-24 13:34:20 -0300

    simple sqlinjection test

---
+++ 1-sqlinjection/index-1.php
@@ -0,0 +1,22 @@
+<?php
+$conn = new PDO("mysql:host=localhost;dbname=database", $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+
+$query = $conn->prepare("SELECT * FROM users WHERE id = '{$_GET['id']}'");
+$query->execute();
+
+$user = $query->fetch(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <label>Username:</label> <span><?= $user['username']; ?></span><br>
+    <label>Email:</label> <span><?= $user['email']; ?></span>
+    <label>Description:</label> <span><?= $user['description']; ?></span>
+</html>
